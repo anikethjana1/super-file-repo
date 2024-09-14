7b63356cc3256e36b41bdad1ac55bbf544c09d28
autonomus-optimus <116957912+autonomus-optimus@users.noreply.github.com>
2024-09-14 22:27:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "")
 #Shortner (token system) 
 # check my discription to help by using my refer link of shareus.io
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "api.shareus.io") 
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "PUIAQBIFrydvLhIzAOeGV8yZppu2")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "") 
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "") # shareus ka tut_vid he 
